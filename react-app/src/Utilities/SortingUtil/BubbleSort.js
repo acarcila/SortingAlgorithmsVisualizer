dc1a662b21fb5230d1705c25c2eff7d312a03117
acarcila <acarcila98@gmail.com>
2020-08-18 19:08:22 -0500

    Modified the steps

--- react-app/src/Utilities/SortingUtil/BubbleSort.js
+++ react-app/src/Utilities/SortingUtil/BubbleSort.js
@@ -4,17 +4,21 @@ export default function (array) {
     for (var i = 0; i < newArray.length - 1; i++) {
         for (var j = 0; j < newArray.length - i - 1; j++) {
             var step = {
-                elements: [j, j + 1],
-                isChange: false
+                elements: []
             }
 
             if (newArray[j] > newArray[j + 1]) {
+                step.elements.push({ index: j, newIndex: j + 1 });
+                step.elements.push({ index: j + 1, newIndex: j + 1 });
                 var temp = newArray[j];
                 newArray[j] = newArray[j + 1];
                 newArray[j + 1] = temp;
-
-                step.isChange = true;
             }
+            else {
+                step.elements.push({ index: j, newIndex: j });
+                step.elements.push({ index: j + 1, newIndex: j + 1 });
+            }
+
             steps.push(step);
         }
     }
