0f7ce28c58c90ed06616a5ffd1b843bcdff24b4f
acarcila <acarcila98@gmail.com>
2020-08-18 19:09:45 -0500

    Programmed the Merge sort

---
+++ react-app/src/Utilities/SortingUtil/MergeSort.js
@@ -0,0 +1,55 @@
+import { arrayMove } from '../ArrayUtil/ArrayUtil';
+
+export default function sort(array, left, right) {
+    var newArray = array.slice(0);      // prevents pass by reference
+    var steps = [];
+
+    if (left < right) {
+        const middle = (parseInt((right - left) / 2) + left);
+
+        const leftArray = sort(newArray, left, middle);
+        newArray = leftArray.newArray;
+        steps = steps.concat(leftArray.steps);
+        const rightArray = sort(newArray, middle + 1, right);
+        newArray = rightArray.newArray;
+        steps = steps.concat(rightArray.steps);
+        const mergedInfo = merge(newArray, left, middle, right);
+        newArray = mergedInfo.newArray
+        steps = steps.concat(mergedInfo.steps);
+
+        console.log(mergedInfo);
+        console.log(steps);
+    }
+    return { newArray, steps };
+}
+
+function merge(array, left, middle, right) {
+    var newArray = array.slice(0);      // prevents pass by reference
+    var steps = [];
+
+    var i = left;
+    var j = middle + 1;
+    if (left == 0 && middle == 4 && right == 9) {
+        console.log();
+    }
+    while (i <= middle && j <= right) {
+        var step = {
+            elements: []
+        }
+        if (newArray[j] < newArray[i]) {
+            step.elements.push({ index: j, newIndex: i });
+            step.elements.push({ index: i+1, newIndex: i+1 });
+            newArray = arrayMove(newArray, j, i);
+            j++
+            middle++;
+        }
+        else
+        {
+            step.elements.push({ index: i, newIndex: i });
+            step.elements.push({ index: j, newIndex: j });
+        }
+        i++;
+        steps.push(step);
+    }
+    return { newArray, steps };
+}
