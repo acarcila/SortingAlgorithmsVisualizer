142f51ca7330b0647ba0a121f6afd43819c42d31
Andrés Camilo Arcila <acarcila98@gmail.com>
2020-12-09 15:27:13 -0500

    Se solucionó deuda técnica

--- react-app/src/Components/ArrayVisualizer/ArrayVisualizer.js
+++ react-app/src/Components/ArrayVisualizer/ArrayVisualizer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Styles from "./ArrayVisualizer.module.sass";
 import ArrayVisualizerElement from "../ArrayVisualizerElement/ArrayVisualizerElement";
 
