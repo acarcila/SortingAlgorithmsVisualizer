1b5ca1616067e7c1246ac819dfe21c695ad1ba4d
Andrés Camilo Arcila <acarcila98@gmail.com>
2020-12-13 00:57:21 -0500

    Se creó un método útil para revolver arreglos

--- react-app/src/Utilities/ArrayUtil/ArrayUtil.js
+++ react-app/src/Utilities/ArrayUtil/ArrayUtil.js
@@ -9,3 +9,11 @@ export function arrayMove(array, oldIndex, newIndex) {
     newArray.splice(newIndex, 0, newArray.splice(oldIndex, 1)[0]);
     return newArray;
 };
+
+export function arrayShuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
